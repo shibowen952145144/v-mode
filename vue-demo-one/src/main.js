faabdf09c6ee792799681f9e66877669a7a49c62
bowe <65216291+shibowen952145144@users.noreply.github.com>
2020-10-14 10:18:02 +0800

    准备工作已完成

--- vue-demo-one/src/main.js
+++ vue-demo-one/src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
-
+    // 引入公共样式
+import './styles/base.less'
+// 自动适配所有屏幕
+import 'lib-flexible'
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: (h) => h(App),
 }).$mount('#app')
